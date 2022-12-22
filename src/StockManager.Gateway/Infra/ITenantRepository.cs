238460791dcc0a409b0b9889381c768c42a306a9
Anna Laura <anna.laura@ndd.tech>
2022-12-22 08:46:49 -0300

    Cria um gateway para resolução do tenant

---
+++ src/StockManager.Gateway/Infra/ITenantRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace StockManager.Gateway.Infra
+{
+    public interface ITenantRepository
+    {
+        object? GetByNameAsync(string tenantName);
+    }
+
+    public class TenantRepository : ITenantRepository
+    {
+        public object? GetByNameAsync(string tenantName)
+        {
+            switch (tenantName)
+            {
+                case "ndd":
+                    return new { Name = "ndd", ConnStr = "abc321" };
+                case "ndd-2":
+                    return new { Name = "ndd-2", ConnStr = "dfg456" };
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ src/StockManager.Gateway/Middleware/TenantResolverMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using StockManager.Gateway.Infra;
+
+namespace StockManager.Gateway.Middleware
+{
+    public class TenantResolverMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantResolverMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context,
+            ITenantRepository repository)
+        {
+            var tenantName = context.Request.Headers["Tenant"];
+
+            var result = repository.GetByNameAsync(tenantName);
+
+            if (result is null)
+            {
+                var error = JsonConvert.SerializeObject(new { error = "Tenant not found" });
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(error);
+            }
+            else
+            {
+                context.Request.Headers["Tenant"] = JsonConvert.SerializeObject(result);
+                await _next(context);
+            }
+        }
+    }
+}
